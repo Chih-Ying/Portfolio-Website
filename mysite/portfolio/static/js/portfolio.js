139adff8b25dbe3b5d5a88aede4d6e20c076c876
ChihYing <>
2019-01-06 20:20:54 +0800

    modified project details: 1. displayed images for Mobile application 2. updated  project 2

--- mysite/portfolio/static/js/portfolio.js
+++ mysite/portfolio/static/js/portfolio.js
@@ -6,4 +6,12 @@ $(function() {
             $(this).css('z-index', 1); 
             $(this).toggleClass('open');    
     }) 
+
+    $('.mobile-wrapper').click(function() {
+        $('.mobile-wrapper').each(function() {
+           $(this).css('z-index', 0); 
+        });
+            $(this).css('z-index', 1); 
+            $(this).toggleClass('open');    
+    }) 
   })
